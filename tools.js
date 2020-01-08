f564491c9db3cdfaa891066d8dad6e4507f22690
eeh123123 <904520880@qq.com>
2020-01-08 14:42:44 +0800

    1

--- tools.js
+++ tools.js
@@ -164,3 +164,30 @@ function ChineseToNumber(chnStr) {
 	}
 	return rtn + section;
 }
+
+//5 数组转tree
+function composeTree(list = []) {
+	//const data = JSON.parse(JSON.stringify(list)) // 浅拷贝不改变源数据
+	const data = list
+	const result = []
+	if (!Array.isArray(data)) {
+		return result
+	}
+	data.forEach(item => {
+		// delete item.children
+		item.children = []
+	})
+	const map = {}
+	data.forEach(item => {
+		map[item.id] = item
+	})
+	data.forEach(item => {
+		const parent = map[item.fatherid]
+		if (parent) {
+			(parent.children || (parent.children = [])).push(item)
+		} else {
+			result.push(item)
+		}
+	})
+	return result
+}
